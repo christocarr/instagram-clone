5f78724c7c126b3be2b7fedc8f68ef2c4827b1f6
christocarr <christocarr@outlook.com>
2021-07-27 11:35:13 +0100

    feat: add infinite loading to pages

--- src/pages/SearchResults/SearchResults.js
+++ src/pages/SearchResults/SearchResults.js
@@ -1,16 +1,20 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import InfiniteLoader from 'react-infinite-loader';
+
 import { PhotoList } from 'components';
 function SearchResults({ location }) {
   const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(1);
   const splitPath = location.pathname.split('/');
   const searchTerm = splitPath[2];
   const getPhotos = async () => {
     try {
       const response = await axios.get(
-        `https://api.unsplash.com/search/photos?client_id=${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}&query=${searchTerm}`
+        `https://api.unsplash.com/search/photos?client_id=${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}&page=${page}&query=${searchTerm}`
       );
-      setPhotos(response.data.results);
+      setPhotos([...photos, ...response.data.results]);
+      setPage(page + 1);
     } catch (err) {
       console.error(err);
     }
@@ -21,9 +25,10 @@ function SearchResults({ location }) {
   }, []);
 
   return (
-    <div>
+    <>
       <PhotoList photos={photos} />
-    </div>
+      <InfiniteLoader onVisited={() => getPhotos()} />
+    </>
   );
 }
 
--- src/pages/User/User.js
+++ src/pages/User/User.js
@@ -1,26 +1,34 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import InfiniteLoader from 'react-infinite-loader';
 import { PhotoList } from 'components';
 
 function User({ match }) {
-  const [user, setUser] = useState([]);
+  const [userPhotos, setUserPhotos] = useState([]);
+  const [page, setPage] = useState(1);
 
-  const getUser = async () => {
+  const getUserPhotos = async () => {
     try {
       const response = await axios.get(
-        `https://api.unsplash.com/users/${match.params.username}/photos/?client_id=${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`
+        `https://api.unsplash.com/users/${match.params.username}/photos/?client_id=${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}&page=${page}`
       );
-      setUser(response.data);
+      setUserPhotos([...userPhotos, ...response.data]);
+      setPage(page + 1);
     } catch (err) {
       console.error(err);
     }
   };
 
   useEffect(() => {
-    getUser();
+    getUserPhotos();
   }, []);
 
-  return <PhotoList photos={user} />;
+  return (
+    <>
+      <PhotoList photos={userPhotos} />
+      <InfiniteLoader onVisited={() => getUserPhotos()} />
+    </>
+  );
 }
 
 export default User;
