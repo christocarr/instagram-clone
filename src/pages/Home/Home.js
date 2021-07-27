7d21d388840ba89e3ac169aef98d7dedade3c100
christocarr <christocarr@outlook.com>
2021-07-27 10:41:42 +0100

    feat: add infinite loading

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,17 +1,20 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import InfiniteLoader from 'react-infinite-loader';
 import { PhotoList } from 'components';
 import { Wrapper } from './Home.Styles';
 
 function Home() {
   const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(1);
 
   const getPhotos = async () => {
     try {
       const response = await axios.get(
-        `https://api.unsplash.com/photos/?client_id=${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`
+        `https://api.unsplash.com/photos/?client_id=${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}&page=${page}`
       );
-      setPhotos(response.data);
+      setPhotos([...photos, ...response.data]);
+      setPage(page + 1);
     } catch (err) {
       console.error(err);
     }
@@ -24,6 +27,7 @@ function Home() {
   return (
     <Wrapper>
       <PhotoList photos={photos} />
+      <InfiniteLoader onVisited={() => getPhotos()} />
     </Wrapper>
   );
 }
