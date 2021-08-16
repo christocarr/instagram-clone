57ca48240b1e4f8fdae503435bee886187dbe286
christocarr <christocarr@outlook.com>
2021-08-16 15:36:19 +0100

    Update SearchResults.js

--- src/pages/SearchResults/SearchResults.js
+++ src/pages/SearchResults/SearchResults.js
@@ -1,11 +1,11 @@
 import InfiniteLoader from 'react-infinite-loader';
-import SaveImage from 'utils/SaveImage';
+import saveImage from 'utils/saveImage';
 import { Wrapper, PhotoList } from 'components';
 
 function SearchResults({ data, getPhotos }) {
   return (
     <Wrapper>
-      <PhotoList photos={data} handleSave={SaveImage} />
+      <PhotoList photos={data} handleSave={saveImage} />
       <InfiniteLoader onVisited={() => getPhotos()} />
     </Wrapper>
   );
