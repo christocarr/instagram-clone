d4763043e34607d567d8f8f7751d5c1fdf3c419b
christocarr <christocarr@outlook.com>
2021-09-09 18:13:27 +0100

    cleanup: remove commmented out css

--- src/components/PhotoList/PhotoList.Styles.js
+++ src/components/PhotoList/PhotoList.Styles.js
@@ -1,19 +1,14 @@
 import styled, { css } from 'styled-components';
 
 export const List = styled.ul`
-  /* display: flex; */
-  /* flex-direction: column; */
-  /* flex-wrap: wrap; */
-  /* justify-content: space-between; */
-
   ${(props) =>
     props.page === 'search'
       ? css`
           display: grid;
           grid-template-columns: repeat(3, 1fr);
           grid-template-rows: repeat(5, 1fr);
-          grid-column-gap: 6px;
-          grid-row-gap: 6px;
+          grid-column-gap: 30px;
+          grid-row-gap: 30px;
         `
       : css``}
 
@@ -21,22 +16,4 @@ export const List = styled.ul`
   margin-right: -5px;
 `;
 
-export const ListItem = styled.li`
-  /* flex: 1 1 auto; */
-  margin-bottom: 5px;
-  /*${(props) =>
-    props.page === 'home'
-      ? css`
-          width: 100%;
-        `
-      : props.page === 'search'
-      ? css`
-          width: 33%;
-        `
-      : css`
-          width: 50%;
-        `} */
-  /* @media (max-width: 768px) {
-    width: 100%;
-  } */
-`;
+export const ListItem = styled.li``;
