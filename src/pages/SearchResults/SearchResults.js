9c581ba40d72f400cb869d6e028c1ac1ba79d6d9
Chris <christocarr@outlook.com>
2021-09-14 13:17:21 +0100

    refactor: add infinite loader component to styled components

--- src/pages/SearchResults/SearchResults.js
+++ src/pages/SearchResults/SearchResults.js
@@ -3,7 +3,19 @@ import { useLocation } from 'react-router-dom';
 import InfiniteLoader from 'react-infinite-loader';
 import saveImage from 'utils/saveImage';
 import { Wrapper, PhotoList } from 'components';
-import { Header, HeaderDescription, HeaderImageContainer, HeaderImage, HashTag, HeaderPara, Posts, LinkNavigation, LinkContainer } from './SearchResults.Styles'
+import {
+  Header,
+  HeaderDescription,
+  HeaderImageContainer,
+  HeaderImage,
+  HashTag,
+  HeaderPara,
+  Posts,
+  LinkNavigation,
+  LinkContainer,
+  InfiniteLoaderContainer,
+  StyledInfinteLoader
+} from './SearchResults.Styles'
 
 function SearchResults({ data, getPhotos }) {
   const [headerData, setHeaderData] = useState(null)
@@ -21,39 +33,34 @@ function SearchResults({ data, getPhotos }) {
     // setSearchType(type)
   }
 
-  console.log(getPhotos)
-
-  //These styles are needed to trigger InfiniteLoader because of list layout
-  const containerStyle = { paddingTop: '2rem', paddingBottom: '2rem', position: 'relative', zIndex: '-100' }
-  const visitStyle = { position: 'absolute', width: '100%', bottom: '10rem', height: '10rem' }
-
   return (
     <Wrapper>
-      <div style={containerStyle}>
-        {headerData && (
-          <Header>
-            <HeaderImageContainer bgColor={headerData.color}>
-              <HeaderImage src={headerData.urls.regular} alt={headerData.alt_description} />
-            </HeaderImageContainer>
-            <HeaderDescription>
-              <HashTag>#{hashTag}</HashTag>
-              <HeaderPara>{headerData.description}</HeaderPara>
-              <Posts> Posts</Posts>
-            </HeaderDescription>
 
-          </Header>
-        )}
-        <LinkNavigation>
-          Top Posts
+      {headerData && (
+        <Header>
+          <HeaderImageContainer bgColor={headerData.color}>
+            <HeaderImage src={headerData.urls.regular} alt={headerData.alt_description} />
+          </HeaderImageContainer>
+          <HeaderDescription>
+            <HashTag>#{hashTag}</HashTag>
+            <HeaderPara>{headerData.description}</HeaderPara>
+            <Posts> Posts</Posts>
+          </HeaderDescription>
+
+        </Header>
+      )}
+      <LinkNavigation>
+        Top Posts
         <LinkContainer>
-            <button onClick={() => handleSearchType('photos')}>Photos</button>
-            <button onClick={() => handleSearchType('collections')}>Collections</button>
-          </LinkContainer>
-        </LinkNavigation>
+          <button onClick={() => handleSearchType('photos')}>Photos</button>
+          <button onClick={() => handleSearchType('collections')}>Collections</button>
+        </LinkContainer>
+      </LinkNavigation>
 
-        <PhotoList photos={data} handleSave={saveImage} />
-        <InfiniteLoader onVisited={getPhotos} visitStyle={visitStyle} />
-      </div>
+      <PhotoList photos={data} handleSave={saveImage} />
+      <InfiniteLoaderContainer>
+        <StyledInfinteLoader onVisited={getPhotos} />
+      </InfiniteLoaderContainer>
     </Wrapper >
   );
 }
--- src/pages/SearchResults/SearchResults.styles.js
+++ src/pages/SearchResults/SearchResults.styles.js
@@ -1,4 +1,5 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import InfiniteLoader from 'react-infinite-loader';
 
 export const Header = styled.div`
   display: flex;
@@ -61,5 +62,19 @@ export const LinkContainer = styled.div`
     margin-right: 1em;
 
   }
-  
-`
+`;
+
+//These styles are needed to trigger InfiniteLoader because of the list layout
+export const InfiniteLoaderContainer = styled.div`
+  position: relative;
+  z-index: -100;
+  padding: 2em 0 2em 0;
+
+`;
+
+export const StyledInfinteLoader = styled(InfiniteLoader)`
+  position: absolute;
+  width: 100%;
+  height: 10em;
+  bottom: 10em;
+`;
