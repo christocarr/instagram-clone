59d25e17b130f3ec084cdca46a0571bc990f981c
Chris <christocarr@outlook.com>
2021-09-27 11:49:03 +0100

    feature: persist state to local storage

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,42 @@ import homePhotos from './homePageReducer/index';
 import explorePhotos from './explorePageReducer/index';
 import searchPhotos from './searchResultsPageReducer/index';
 import userData from './userPageReducer/index';
+import togglePhoto from './toggleSavePhotoReducer/index';
+
+function saveToLocalStorage(state) {
+  try {
+    const serialisedState = JSON.stringify(state);
+    localStorage.setItem('state', serialisedState);
+  } catch (err) {
+    console.warn(err);
+  }
+}
+
+function loadFromLocalStorage() {
+  try {
+    const serialisedState = localStorage.getItem('state');
+    if (serialisedState === null) return undefined;
+    return JSON.parse(serialisedState);
+  } catch (err) {
+    console.warn(err);
+    return undefined;
+  }
+}
 
 const reducers = combineReducers({
   homePhotos,
   explorePhotos,
   searchPhotos,
   userData,
+  togglePhoto,
 });
 
-export const store = createStore(reducers, applyMiddleware(thunk));
+const store = createStore(
+  reducers,
+  loadFromLocalStorage(),
+  applyMiddleware(thunk)
+);
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
+
+export default store;
